3d0496aa4913cf828b6373a85285b71b67333acd
Julian Haldenby <j.haldenby@gmail.com>
2019-06-23 02:14:04 -0400

    Increasing compatibility

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exda.MixProject do
     [
       app: :exda,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
